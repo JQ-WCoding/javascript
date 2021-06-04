f22965505c24edf43638d4fe946dde747b18315e
SpellBound <junlee36520620@gmail.com>
2021-06-04 23:24:11 +0900

    innerHtml
    button click

---
+++ script/display/inner.js
@@ -0,0 +1,11 @@
+// id button tag 에 해당 html 글이 직접 입력된다.
+function myFunction() {
+    document.getElementById( "button" ).innerHTML = "button was clicked";
+}
+
+// write
+function writeFunction() {
+    document.write( "button was clicked" );
+}
+
+
