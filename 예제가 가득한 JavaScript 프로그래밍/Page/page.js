fe4de6c2f83d85c94e88bb165ad01302fe3085f3
SpellBound <junlee36520620@gmail.com>
2021-06-26 23:26:56 +0900

    JavaScript

--- 예제가 가득한 JavaScript 프로그래밍/Page/page.js
+++ 예제가 가득한 JavaScript 프로그래밍/Page/page.js
@@ -1,24 +1,24 @@
 // 앞페이지가 이력에 남음
-location.href("location");
+location.href( "location" );
 
 // 앞페이지가 이력에 남지 않음
-location.replace("location");
+location.replace( "location" );
 
 // 이력에 현재 페이지 상태 저장하기
-history.pushState('state' + 1, "state" + 1);
+history.pushState( 'state' + 1, "state" + 1 );
 
 //JSON parsing
 var str = {
-    'title' : "JAVA"
-    , 'price'  : "3500"
+    'title': "JAVA"
+    , 'price': "3500"
 }
-var obg = $.parseJSON(str);
-console.log(obj.title);
+var obg = $.parseJSON( str );
+console.log( obj.title );
 
 // 페이지가 모두 읽힌 뒤
-$(function (  ){
+$( function () {
 
-});
+} );
 
 $.addClass(); // 클래스 적용하기
 $.removeClass(); // 클래스 제거하기
@@ -28,5 +28,11 @@ $.toggleClass(); // 클래스 적용 혹은 제외하기
 $.height();
 $.width();
 
+var data = {
+    'page': 3,
+    'common': 4
+}
+
+console.log( data.page );
 
 
